429f4430f3f929328cc435df77c638899a916f89
FedericoLedesma <federledesma10@gmail.com>
2021-07-04 18:11:47 -0300

    Se agregaron los controladores para Restriccion y ArmarPCFlujo

---
+++ src/tienda_virtual/controllers/backoffice/categories/ArmarPCFlujoController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace src\tienda_virtual\controllers\backoffice\categories;
+
+use src\tienda_virtual\controllers\backoffice\ABMController;
+use src\tienda_virtual\database\services\DatabaseService;
+
+
+class ArmarPCFlujoController extends ABMController
+
+{
+    public function __construct()
+    {
+        parent::__construct("ArmarPCFlujoService", "backoffice.abm");
+    }
+}
