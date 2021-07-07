8cff3b1d56d0e222a807e72810baf3b79c70f58d
Nicolás Larena <nic.lar.universidad@gmail.com>
2021-07-07 07:57:19 -0300

    Armá tu PC flujo

--- src/tienda_virtual/controllers/armartupc/ArmarTuPcController.php
+++ src/tienda_virtual/controllers/armartupc/ArmarTuPcController.php
@@ -6,39 +6,27 @@ namespace src\tienda_virtual\controllers\armartupc;
 
 use src\tienda_virtual\database\services\categories\ArmarPCFlujoService;
 use src\tienda_virtual\database\services\products\ProductoService;
+use src\tienda_virtual\services\ArmarPCService;
 use src\tienda_virtual\services\TwigPageFinderService;
 
 class ArmarTuPcController extends \src\tienda_virtual\controllers\Controller
 {
-    protected ArmarPCFlujoService $flujoService;
-    protected ProductoService $productoService;
+    protected ArmarPCService $service;
     public function init()
     {
         parent::init();
         $this->pageFinderService = new TwigPageFinderService();
         $this->pageFinderService->session = $this->session;
-        $this->flujoService = new ArmarPCFlujoService($this->connection, $this->logger);
-        $this->productoService = new ProductoService($this->connection, $this->logger);
+        $this->service = new ArmarPCService();
+        $this->service->setSession($this->session);
+        $this->service->setConnection($this->connection);
+        $this->service->setLogger($this->logger);
+        $this->service->init();
     }
     public function mostrarTemplate(String $notificacion = "", array $data = [], String $titulo = "Armá tu PC") {
-        $paso = $this->request->get("paso");
-        $pasos = $this->flujoService->findAll();
-        if (!isset($paso) && (count($pasos) > 0)) {
-            $paso = $pasos[0]["id"];
-        }
-        $actual = null;
-        if (isset($paso)) {
-            $actual = $this->flujoService->find($paso)[0];
-            $productos = $this->productoService->findBySubCategoriaId($actual["id_sub_categoria"]);
-            if (count($productos) > 0) {
-                $data["productos"] = $productos[0];
-            } else {
-                $data["productos"] = [];
-            }
-
-        }
-        $data["actual"] = $actual;
-        $data["pasos"] = $pasos;
+        $paso = $this->request->get("paso") ?? "";
+        $idProducto = $this->request->get("producto") ?? "";
+        $data = $this->service->procesarPaso($paso, $idProducto, $data);
         $this->pageFinderService->findFileRute("armar.tu.pc","twig","twig", [],
             $data,$titulo, []);
     }
