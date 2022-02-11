9e261ee929a334050e1e723a6a2f9c196df4e284
dbm349 <delmore97@hotmail.com>
2022-02-11 15:08:26 -0300

    agrego OfertasModel

---
+++ src/tienda_virtual/database/models/ofertas/OfertasModel.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace src\tienda_virtual\database\models\ofertas;
+
+
+use src\tienda_virtual\database\models\Model;
+
+class OfertasModel extends Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setField("precio_oferta", "");
+        $this->setField("activa", "");
+        $this->setField("id_publicacion", "");
+    }
+}
