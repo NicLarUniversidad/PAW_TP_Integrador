bc3b58d709027e74d43f8e3a4334a147d3880ae0
Nico Larena <nic.lar.universidad@gmail.com>
2022-02-12 14:50:22 -0300

    ABM Ofertas

--- src/tienda_virtual/database/services/products/OfertaService.php
+++ src/tienda_virtual/database/services/products/OfertaService.php
@@ -7,9 +7,12 @@ use src\tienda_virtual\database\services\DatabaseService;
 
 class OfertaService extends DatabaseService
 {
+    private PublicacionService $publicacionService;
+    
     public function __construct(PDO $PDO, Logger $logger)
     {
         parent::__construct($PDO, $logger, "products\\OfertaRepository");
+        $this->publicacionService = new PublicacionService($PDO, $logger);
     }
     
     public function attachData(array $data = []): array
@@ -18,7 +21,9 @@ class OfertaService extends DatabaseService
     }
     
     public function attachInsertData(array $data = []) : array {
-        return $this->attachMetadata(parent::attachInsertData($data));
+        $data = $this->attachMetadata(parent::attachInsertData($data));
+        $data = $this->formatFieldNameInsert($data, "precio_oferta", "Precio oferta");
+        return $this->formatFieldNameInsert($data, "id_publicacion", "Publicacion");
     }
     
     public function attachMetadata(array $data) : array {
@@ -27,10 +32,21 @@ class OfertaService extends DatabaseService
         $data["item-url"] = "backoffice-ofertas-item";
         $data["insert-url"] = "backoffice-ofertas-insert";
         $data["register"]["title"] = "Agregar Oferta";
+        $data = $this->dataSetSelect($data, "id_publicacion", $this->buildOptionsPublicacion());
         return $this->dataSetSelect($data,"activo",[
             "SI"=>"SI",
             "NO"=>"No"
         ]);
+    }
+    
+    private function buildOptionsPublicacion(): array
+    {
+        $options = [];
+        $tuples = $this->publicacionService->findAll();
+        foreach ($tuples as $tuple) {
+            $options[(string)$tuple["id"]] = $tuple["descripcion"];
+        }
+        return $options;
     }
 }
 
