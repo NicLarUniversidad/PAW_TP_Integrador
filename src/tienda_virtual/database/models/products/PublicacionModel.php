617122ec66671266cc1e8494272de64420b9946f
dbm349 <delmore97@hotmail.com>
2022-10-09 17:43:52 -0300

    elimino fecha_entrada de publicacion

--- src/tienda_virtual/database/models/products/PublicacionModel.php
+++ src/tienda_virtual/database/models/products/PublicacionModel.php
@@ -11,7 +11,7 @@ class PublicacionModel extends Model
     public function __construct()
     {
         parent::__construct();
-        $this->setField("fecha_entrada", "");
+        /*$this->setField("fecha_entrada", "");*/
         $this->setField("cantidad_inicial", "");
         $this->setField("id_producto", "");
         $this->setField("precio_unidad", "");
