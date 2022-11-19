bdd7348f4666d25e8119d7fab35dbad3d984a56e
dbm349 <delmore97@hotmail.com>
2022-11-19 20:37:57 -0300

    agrego contenido a menu financiero

--- public/js/scripts/pagos.js
+++ public/js/scripts/pagos.js
@@ -1,8 +1,22 @@
-function abrir(){
-    document.getElementById("vent").style.display="block";
+function abrirPromocionesBancarias(){
+    document.getElementById("ventPromocionesBancarias").style.display="block";
+}
+function abrirMediosDePago(){
+    document.getElementById("ventMediosDePago").style.display="block";
 }
 
+function abrirEfectivo(){
+    document.getElementById("ventEfectivo").style.display="block";
+}
 
-function cerrar(){
-    document.getElementById("vent").style.display="none";
+function cerrarPromocionesBancarias(){
+    document.getElementById("ventPromocionesBancarias").style.display="none";
+}
+
+function cerrarMediosDePago(){
+    document.getElementById("ventMediosDePago").style.display="none";
+}
+
+function cerrarEfectivo(){
+    document.getElementById("ventEfectivo").style.display="none";
 }
