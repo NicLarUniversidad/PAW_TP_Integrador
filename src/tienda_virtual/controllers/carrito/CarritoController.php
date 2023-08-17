6021bf10d778751845d0ac5a743acddc01e16d4f
Nico Larena <nicolas.larena.work@gmail.com>
2023-08-18 01:43:17 +0900

    Redirect, add product

--- src/tienda_virtual/controllers/carrito/CarritoController.php
+++ src/tienda_virtual/controllers/carrito/CarritoController.php
@@ -9,11 +9,13 @@ use MercadoPago\Preference;
 use src\tienda_virtual\controllers\Controller;
 use src\tienda_virtual\database\services\carrito\CarritoService;
 use src\tienda_virtual\services\TwigPageFinderService;
+use src\tienda_virtual\services\UserService;
 
 class CarritoController extends Controller
 {
     protected CarritoService $carritoService;
     protected Preference $preference;
+    protected UserService $userService;
 
     public function init()
     {
@@ -23,6 +25,7 @@ class CarritoController extends Controller
         $this->carritoService = new CarritoService($this->connection, $this->logger);
         $this->carritoService->setSession($this->session);
         $this->carritoService->init();
+        $this->userService = new UserService($this->connection, $this->logger);
         //$this->preference = new Preference();
     }
 
@@ -30,17 +33,32 @@ class CarritoController extends Controller
      * @throws Exception
      */
     public function mostrarTemplate(String $notificacion = "", array $data = [], String $titulo = "Carrito") {
-        $cssImports = [];
-        $cssImports[] = "main";
-        $cssImports[] = "carrito";
-        $jsImports = [];
-        $jsImports[]="paw";
-        $jsImports[]="app";
-        $data = $this->carritoService->findItems($data);
-        //$this->preference->save();
-        $data["preference"] = $this->preference ?? [];
-        $this->pageFinderService->findFileRute("carrito","twig","twig", $cssImports,
-            $data,$titulo, $jsImports);
+        $loggedUser = $this->userService->getLoggedUser();
+        $publicationId = $this->request->get("publicacion_id");
+        if (is_null($loggedUser)) {
+            $url = "login?redirect=carrito";
+            if (!is_null($publicationId)) {
+                //Hay que usar encoding para caracteres especiales en URL...
+                //? %3F
+                //= %3D
+                $url .= "%3Fpublicacion_id%3D".$publicationId;
+            }
+            header("Location: login?redirect%=$url");
+        }
+        else {
+            $cssImports = [];
+            $cssImports[] = "main";
+            $cssImports[] = "carrito";
+            $jsImports = [];
+            $jsImports[]="paw";
+            $jsImports[]="app";
+            $data = $this->carritoService->findItems($data);
+            $this->carritoService->addItem($publicationId);
+            //$this->preference->save();
+            $data["preference"] = $this->preference ?? [];
+            $this->pageFinderService->findFileRute("carrito","twig","twig", $cssImports,
+                $data,$titulo, $jsImports);
+        }
     }
 
     /**
